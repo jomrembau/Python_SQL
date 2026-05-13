e1d878cd3e3b4d64b80e33c5675969b97a0f58e6
jomrembau <jomir.bautista@gmail.com>
2026-05-13 08:19:10 +0200

    Interface done

---
+++ Day 1/Challenges/Challenge 1 - Interface.py
@@ -0,0 +1,43 @@
+def interface():
+    print("\nWelcome to Freight Manager")
+
+    print("""
+            1. Add a box type
+            2. Show box types
+            3. Load box to container
+            4. Show container
+            5. Summary Report
+            x. Close
+            """)
+
+    n = 0
+
+    while n != "x":
+        n = input("\nYour Choice: ").lower()
+
+        match n:
+            case "1":
+                print("Choice 1 Selected")
+
+            case "2":
+                print("Choice 2 Selected")
+
+            case "3":
+                print("Choice 3 Selected")
+
+            case "4":
+                print("Choice 4 Selected")
+
+            case "5":
+                print("Choice 5 Selected")
+
+            case "x":
+                print("Goodbye")
+
+            case _:
+                print("Invalid input")
+
+if __name__ == "__main__":
+    interface()
+
+
